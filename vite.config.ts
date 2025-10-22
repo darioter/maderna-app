e4a9f91d133cb5742ae0dc5e602c0e0d8b72a578
darioter <84633130+darioter@users.noreply.github.com>
2025-10-22 20:46:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-// Ajustá 'base' si cambiás el nombre del repo
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? "/maderna-app/" : "/",
+  // ⚠️ Si el repo NO se llama "maderna-app", cambiá el segmento de abajo
+  base: process.env.GITHUB_PAGES ? '/maderna-app/' : '/',
   build: { sourcemap: true },
-});
+})
