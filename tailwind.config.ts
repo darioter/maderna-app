bd4aca021545d88f172f35026cd8da10b619e61d
darioter <84633130+darioter@users.noreply.github.com>
2025-10-22 20:45:01 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from 'tailwindcss'
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: { extend: {} },
   plugins: [],
-} satisfies Config
+};
