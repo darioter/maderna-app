26f12b5eda5417005fdff8c2338f23851516f325
darioter <84633130+darioter@users.noreply.github.com>
2025-10-23 09:54:17 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
+
