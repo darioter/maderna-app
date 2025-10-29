35a7411ed7c1e9897e320d8fbfcba9dc4d3054ca
darioter <84633130+darioter@users.noreply.github.com>
2025-10-29 10:46:49 -0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // src/lib/config.ts
-export const DRIVE_SYNC_URL = 'PEGAR_AQUI_TU_WEB_APP_URL'; // <- el URL del Deploy
-export const DRIVE_SYNC_KEY = 'PEGAR_AQUI_TU_MISMA_SECRET'; // <- misma clave del Apps Script
+export const DRIVE_SYNC_URL = 'https://script.google.com/macros/s/AKfycbzFCiZqDGxNnrYFyWEecYMy-8ivzluFlrgCvq0vVmRjGpzidNRZkFMIH1SD1b2q97ij/exec'; // <- el URL del Deploy
+export const DRIVE_SYNC_KEY = 'ESQUINA2025'; // <- misma clave del Apps Script
 export const DRIVE_SYNC_ENABLED = true; // podés apagarlo si querés volver a solo-local
