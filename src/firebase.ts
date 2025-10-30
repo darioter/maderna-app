94cf0baee541d039e78dd3cf718e232798b85316
darioter <84633130+darioter@users.noreply.github.com>
2025-10-30 16:23:20 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
-// src/firebase.ts
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDezDfFOozeT8HR_Nslg1xihLFwT8SwCSs",
@@ -8,9 +7,8 @@ const firebaseConfig = {
   projectId: "maderna-app",
   storageBucket: "maderna-app.firebasestorage.app",
   messagingSenderId: "816020564615",
-  appId: "1:816020564615:web:d5c0d3a84f2b8b55866622",
-};
+  appId: "1:816020564615:web:d5c0d3a84f2b8b55866622"
+}
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
