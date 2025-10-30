a01449281cd879321eb2cc5bf9c52b5e3b4c1f99
darioter <84633130+darioter@users.noreply.github.com>
2025-10-30 11:13:35 -0300

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDezDfFOozeT8HR_Nslg1xihLFwT8SwCSs",
+  authDomain: "maderna-app.firebaseapp.com",
+  projectId: "maderna-app",
+  storageBucket: "maderna-app.firebasestorage.app",
+  messagingSenderId: "816020564615",
+  appId: "1:816020564615:web:d5c0d3a84f2b8b55866622",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
