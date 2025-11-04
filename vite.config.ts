418de076672ac58a17b9cb078e6dc47bdbbbb0aa
darioter <84633130+darioter@users.noreply.github.com>
2025-11-04 12:56:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES ? '/maderna-app/' : '/',
-  build: { sourcemap: true },
 })
+
